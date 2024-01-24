ed895f86fc6ac3640cadd1f909f99603137ddf7c
Alex Tsaparas <up1072824@upnet.gr>
2024-01-24 04:08:45 +0100

    Copy the template of another githuber

--- src/main/java/com/tsaperlein/todolist/App.java
+++
@@ -1,9 +0,0 @@
-// App.java
-package main.java.com.tsaperlein.todolist;
-
-public class App {
-    public static void main(String[] args) {
-        ToDoListManager toDoListManager = new ToDoListManager();
-        toDoListManager.start();
-    }
-}
